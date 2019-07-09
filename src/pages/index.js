2c0523e85b625e3ec0fc06a6f735769412cb9196
Edward Weymouth <contextcue@Edwards-MacBook-Pro.local>
2019-07-09 11:37:47 -0600

    added in redux

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,36 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { connect } from "react-redux"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const Counter = ({ count, increment }) => (
+  <div>
+    <p>Count: {count}</p>
+    <button onClick={increment}>Increment</button>
+  </div>
+)
+
+Counter.propTypes = {
+  count: PropTypes.number.isRequired,
+  increment: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = ({ count }) => {
+  return { count }
+}
+
+const mapDispatchToProps = dispatch => {
+  return { increment: () => dispatch({ type: `INCREMENT` }) }
+}
+
+const ConnectedCounter = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
 
 const IndexPage = () => (
   <Layout>
@@ -11,6 +38,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <ConnectedCounter />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
